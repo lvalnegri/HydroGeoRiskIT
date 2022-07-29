8151f70e2520ce3cac45128ce3bdd9d7f49027fd
datamaps <l.valnegri@datamaps.co.uk>
2022-07-29 17:36:23 +0100

    commit

--- R/map_risks.R
+++ R/map_risks.R
@@ -8,9 +8,9 @@
 #' @param neigh     IF `TRUE`, also includes all neighbours of the specified District(s)
 #' @param cmn       If `TRUE`, also draws the polygons of the District(s)
 #' @param cmnv      If `TRUE` (and also `cmn`is `TRUE`), adds the polygons of the neighbours of the District(s) 
-#' @param mps       If `TRUE`, returns a complete map; 
+#' @param with_mps  If `TRUE`, returns a complete map; 
 #'                  otherwise only the required polygons and the layers' menu 
-#' @param maptile   If `NULL` keep the default maptile `CartoDB.Positron`, 
+#' @param add_tile  If `NULL` keep the default maptile `CartoDB.Positron`, 
 #'                  otherwise a 
 #' @param lvl_nms   If `NULL` keep the default names for the levels of the specified risk, 
 #'                  otherwise a character vector of the required length with the desired names
@@ -25,6 +25,7 @@
 #' @param 
 #' @param 
 #' @param add_tbl   Add a table as label with information about people 
+#' @param str_title A title to add to the map 
 #' @param verbose 
 #' 
 #' @return un oggetto `leaflet`
@@ -36,41 +37,42 @@
 #'
 #' @export
 #'
-mappa_poli_comune <- function(x, risk,
-                              neigh = FALSE, 
-                              cmn = TRUE, 
-                              cmnv = FALSE, 
-                              mps = TRUE,
-                              maptile = NULL,
-                              lvl_nms = NULL,
-                              lvl_cls = NULL,
-                              poly_tsp = 0.2,
-                              
-                              
-                              
-                              
-                              
-                              add_tbl = TRUE,
-                              verbose = FALSE
-                              
+map_risks <- function(x, risk,
+                  neigh = FALSE, 
+                  cmn = TRUE, 
+                  cmnv = FALSE, 
+                  with_mps = TRUE,
+                  add_tile = NULL,
+                  lvl_nms = NULL,
+                  lvl_cls = NULL,
+                  poly_tsp = 0.2,
+                  
+                  
+                  
+                  
+                  
+                  add_tbl = TRUE,
+                  verbose = FALSE
     ){
     
         # LETTURA FILE POLIGONI+TABELLA
         y <- qread(file.path(dpath, risk, paste0(x, ifelse(neigh, 'v', ''))), nthreads = 6)
         
-        if(is.na(y$bbx)){
+        if(is.null(y$bbx)){
             if(verbose) stop('The specified District has no hazard areas for the required risk.')
             return(NULL)
         }
+        
+        ym <- if(with_mps) mps else NULL
         
         grps <- NULL
         yr <- risks.lst[[risk]]
         for(yrn in names(yr)){
             if(!is.null(y[[yrn]])){
-                yrc <- palette.lst[[input$rdb_rsk]][[yrn]]
-                grp <- risks.lst[[input$rdb_rsk]][[yrn]]
+                yrc <- palette.lst[[risk]][[yrn]]
+                grp <- risks.lst[[risk]][[yrn]]
                 grps <- c(grps, grp)
-                y <- y |> 
+                ym <- ym |> 
                         addPolygons(
                             data = y[[yrn]], 
                             group = grp, 
@@ -83,39 +85,39 @@ mappa_poli_comune <- function(x, risk,
         
         # POLIGONO COMUNE
         if(cmn){
-            y <- y |> 
-                addPolygons(
-                    data = y$CMN,
-                    weight = 3,
-                    color = 'red',
-                    opacity = 1,
-                    fillOpacity = 0,
-                    label = if(add_tbl) y$dts else NULL,
-                    highlightOptions = hlt.options
-                )
+            ym <- ym |> 
+                    addPolygons(
+                        data = y$CMN,
+                        weight = 3,
+                        color = 'red',
+                        opacity = 1,
+                        fillOpacity = 0,
+                        label = if(add_tbl) y$dts else NULL,
+                        highlightOptions = hlt.options
+                    )
         }
         
         # POLIGONI COMUNI LIMITROFI
         if(cmnv){
-            y <- y |> 
-            addPolygons(
-                data = y$CMNv,
-                weight = 2,
-                color = 'black',
-                opacity = 1,
-                fillOpacity = 0,
-                label = if(add_tbl) y$dtsv else NULL,
-                highlightOptions = hlt.options
-            )
+            ym <- ym |> 
+                    addPolygons(
+                        data = y$CMNv,
+                        weight = 2,
+                        color = 'black',
+                        opacity = 1,
+                        fillOpacity = 0,
+                        label = if(add_tbl) y$dtsv else NULL,
+                        highlightOptions = hlt.options
+                    )
         }
         
         # TITOLO
-        if(ttl){
-            # y <- y |> addControl()
+        if(str_title){
+            # ym <- ym |> addControl()
         }    
         
         # MENU
-        y |> 
+        ym |> 
             addLayersControl(
                 overlayGroups = grps, 
                 options = layersControlOptions(collapsed = FALSE)
